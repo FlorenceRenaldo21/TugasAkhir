8816cfac79726347764671a9a4d447ac83c85152
samje <renaldo.florence@mhs.mdp.ac.id>
2024-06-04 11:22:17 +0700

    Init Commit

---
+++ app/src/main/java/com/example/tugasakhir/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.tugasakhir;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.tugasakhir.databinding.ActivityMainBinding;
+
+
+public class MainActivity extends AppCompatActivity {
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, CameraActivity.class));
+            }
+        });
+    }
+}
